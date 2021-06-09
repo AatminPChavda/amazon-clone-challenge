384796d9ca7ac34848b0f072be84d84e3cc1c57c
AatminPChavda <aatminchavda@gmail.com>
2021-06-09 14:06:48 +0300

    Initializing Repo

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA8PydDJdpqCvoKgNcTLrYzmEDwUSfkfd4",
+    authDomain: "clone-challenge-fefc3.firebaseapp.com",
+    projectId: "clone-challenge-fefc3",
+    storageBucket: "clone-challenge-fefc3.appspot.com",
+    messagingSenderId: "815170559268",
+    appId: "1:815170559268:web:558b518e8baddae14767b1",
+    measurementId: "G-6KPXZMDJJG"
+  };
